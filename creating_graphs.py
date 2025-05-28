92d084539b970f76c7b82793a6fe0f1b77057044
Dylan Serrarens <121444972+DylanSirr@users.noreply.github.com>
2025-05-28 18:06:53 +0200

    make_even

--- creating_graphs.py
+++ creating_graphs.py
@@ -9,3 +9,19 @@ def generate_hamiltonian_cycle(n):
         b = verts[(i+1) % n]
         edges.add(tuple(sorted((a,b))))
     return edges
+
+def make_even(edges, n):
+    deg = [0]*n 
+
+    for a,b in edges:
+        deg[a] += 1
+        deg[b] += 1
+    
+    odd = [i for i, d in enumerate(deg) if d % 2 == 1]
+
+    while len(odd) >= 3:
+        a,b,c = odd[:3]
+        edges.add(tuple(sorted((a,b))))  
+        edges.add(tuple(sorted((b,c))))
+        edges.add(tuple(sorted((c,a))))
+        odd = odd[3:]
